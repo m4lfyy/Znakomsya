924e9a12df2639c0a0ee059464d02e9aaab56748
m4lfyy <pashapanchuk@gmail.com>
2023-12-29 23:22:30 +0300

    Initial Commit

---
+++ Znakomsya/ZnakomsyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZnakomsyaApp.swift
+//  Znakomsya
+//
+//  Created by Павел Панчук on 29.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ZnakomsyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
