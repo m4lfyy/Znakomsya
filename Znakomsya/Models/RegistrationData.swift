d851d756f3a0d08c4681ee880765193654fedc57
DanilYusupov03 <danil.yusupov03@gmail.com>
2024-03-26 17:06:37 +0300

    feat: reg view1

--- Znakomsya/Models/RegistrationData.swift
+++ Znakomsya/Models/RegistrationData.swift
@@ -2,11 +2,66 @@ import SwiftUI
 
 struct RegistrationData {
     var name: String = ""
-    var birthDate: Date = Date()
     var gender: String = "М"
-    var city: String = "Москва"
-    var username: String = ""
-    var photo: Image?
+    var birthDate: Date = Date()
+    var email: String = ""
+    var phone: String = ""
     var password: String = ""
+    
+    func validationError() -> String? {
+        if !isValidAge() {
+            return "Пользователь должен быть не младше 16 лет"
+        }
+
+        if !isValidPhoneNumber() {
+            return "Неверный номер телефона"
+        }
+
+        if !isValidEmail() {
+            return "Неверный адрес электронной почты"
+        }
+
+        if name.isEmpty {
+            return "Имя пользователя должно быть заполнено"
+        }
+
+        return nil
+    }
+
+    private func isValidAge() -> Bool {
+        let currentDate = Date()
+        let calendar = Calendar.current
+        if let age = calendar.dateComponents([.year], from: birthDate, to: currentDate).year {
+            return age >= 16
+        }
+        return false
+    }
+
+    private func isValidPhoneNumber() -> Bool {
+        let phoneRegex = "^\\+7\\d{10}$"
+        let predicate = NSPredicate(format: "SELF MATCHES %@", phoneRegex)
+        return predicate.evaluate(with: phone)
+    }
+
+    private func isValidEmail() -> Bool {
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let predicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        return predicate.evaluate(with: email)
+    }
 }
 
+extension RegistrationData: Encodable {
+    enum CodingKeys: String, CodingKey {
+        case name, gender, birthDate, email, phone, password
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+        try container.encode(gender, forKey: .gender)
+        try container.encode(birthDate, forKey: .birthDate)
+        try container.encode(email, forKey: .email)
+        try container.encode(phone, forKey: .phone)
+        try container.encode(password, forKey: .password)
+    }
+}
--- Znakomsya/Views/RegistrationView.swift
+++ Znakomsya/Views/RegistrationView.swift
@@ -10,79 +10,181 @@ struct RegistrationView: View {
     
     var body: some View {
         ZStack {
-            Group {
-                Image("bg_img")
-                    .resizable()
-                    .edgesIgnoringSafeArea(.all)
-                // Окно ввода
-                Rectangle()
-                    .foregroundColor(.clear)
-                    .frame(width: 340, height: 505)
-                    .background(Color(red: 1, green: 0.90, blue: 0.90).opacity(0.35))
-                    .cornerRadius(30)
-                    .offset(x: 0.50, y: 30.50)
-                // Кнопка регистрации
-                Rectangle()
-                    .foregroundColor(.clear)
-                    .frame(width: 280, height: 49)
-                    .background(Color(red: 1, green: 0.89, blue: 0.89).opacity(0.60))
-                    .cornerRadius(15)
-                    .offset(x: 0.50, y: 223.50)
-                // Заголовок
+            Image("bg_img")
+                .resizable()
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                .scaledToFill()
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+            
+            VStack (spacing: 30) {
                 Text("Регистрация")
                     .frame(width: 180, height: 43)
-                    .offset(y:-285)
                     .font(Font.custom("Montserrat-Medium", size: 26))
                     .foregroundColor(Color(red: 0.22, green: 0.23, blue: 0.25))
-            }
-            VStack {
-                // Имя
-                ZStack(alignment: .leading) {
-                    Image("name_img")
-                        .offset(x: -15, y: -175)
-                    // Затычка на placeholder (заработало без нее)
-                    /*if modelData.registrationData.name.isEmpty {
-                        Text("Имя")
-                            .foregroundColor(Color(red: 0.28, green: 0.28, blue: 0.29).opacity(0.70))
-                            .font(Font.custom("Montserrat-Meduim", size: 18))
-                            .frame(height: 50)
-                            .offset(x: 20, y: -174.50)
-                    }*/
-                    TextField("Введите имя", text: $modelData.registrationData.name)
-                        .font(Font.custom("Montserrat-Meduim", size: 18))
-                        .foregroundColor(Color(red: 0.22, green: 0.23, blue: 0.25))
-                        .frame(width: 230, height: 50)
-                        .offset(x: 20, y: -175)
-
-                    // Пол
-                    Image("male_img")
-                        .offset(x: -15, y: -114.50)
-                        .opacity(0.6)
-                    
-                        Picker("", selection: $modelData.registrationData.gender) {
-                            Text("Мужской").tag("М")
-                            Text("Женский").tag("Ж")
-                        }
-                        .pickerStyle(SegmentedPickerStyle())
-                        .frame(width: 200)
-                        .offset(x:13, y: -114.50)
-                    
-                    Image("female_img")
-                        .offset(x: 218, y: -114.50)
-                        .opacity(0.6)
-                }
-
-                Divider()
-                    .frame(width: 280)
-                    .background(Color.black)
-                    .opacity(0.6)
-                    .offset(y: -180)
                 
+                ZStack {
+                    Rectangle()
+                        .foregroundColor(.clear)
+                        .frame(width: 340, height: 505)
+                        .background(Color(red: 1, green: 0.90, blue: 0.90).opacity(0.35))
+                        .cornerRadius(30)
+                    
+                    // Стек для заполняемых полей
+                    VStack {
+                        // Стек для имени
+                        VStack (alignment: .leading){
+                            HStack (spacing: 10) {
+                                Image("name_img")
+                                TextField("Введите имя", text: $modelData.registrationData.name)
+                                    .font(Font.custom("Montserrat-Meduim", size: 18))
+                                    .foregroundColor(Color(red: 0.22, green: 0.23, blue: 0.25))
+                                    .frame(width: 230)
+                            }
+                            Rectangle ()
+                                .frame(width: 280, height: 1)
+                                .foregroundColor(.clear)
+                                .background(Color(.black).opacity(0.4))
+                        }
+                        .padding(.horizontal)
+                        .padding(.bottom, 10)
+                        
+                        // Стек для пола
+                        VStack {
+                            HStack (spacing: 10) {
+                                Image("male_img")
+                                    .opacity(0.6)
+                                
+                                Picker("", selection: $modelData.registrationData.gender) {
+                                    Text("Мужской").tag("М")
+                                    Text("Женский").tag("Ж")
+                                }
+                                .pickerStyle(SegmentedPickerStyle())
+                                .frame(width: 208)
+                                
+                                Image("female_img")
+                                    .opacity(0.6)
+                            }
+                            Rectangle ()
+                                .frame(width: 280, height: 1)
+                                .foregroundColor(.clear)
+                                .background(Color(.black).opacity(0.4))
+                        }
+                        .padding(.horizontal)
+                        .padding(.bottom, 8)
+                        
+                        // Стек для даты
+                        VStack (alignment: .leading){
+                            HStack (spacing: 10) {
+                                Image("calendar_img")
+                                    .opacity(0.6)
+                                DatePicker ("Дата ", selection: $modelData.registrationData.birthDate, in: ...Date(), displayedComponents: .date)
+                                    .frame(width: 220)
+                                    .font(.custom("Montserrat-Light", size: 16))
+                                    .foregroundColor(Color(red: 0.28, green: 0.28, blue: 0.29).opacity(0.70))
+                                    .multilineTextAlignment(.center)
+                            }
+                            Rectangle ()
+                                .frame(width: 280, height: 1)
+                                .foregroundColor(.clear)
+                                .background(Color(.black).opacity(0.4))
+                        }
+                        .padding(.horizontal)
+                        .padding(.bottom, 10)
+                        
+                        // Стек для почты
+                        VStack (alignment: .leading){
+                            HStack (spacing: 10) {
+                                Image("mail_img")
+                                    .opacity(0.6)
+                                TextField("Введите Почту", text: $modelData.registrationData.email)
+                                    .font(Font.custom("Montserrat-Meduim", size: 18))
+                                    .foregroundColor(Color(red: 0.22, green: 0.23, blue: 0.25))
+                                    .frame(width: 230)
+                            }
+                            Rectangle ()
+                                .frame(width: 280, height: 1)
+                                .foregroundColor(.clear)
+                                .background(Color(.black).opacity(0.4))
+                        }
+                        .padding(.horizontal)
+                        .padding(.bottom, 10)
+                        
+                        // Стек для телефона
+                        VStack (alignment: .leading){
+                            HStack (spacing: 10) {
+                                Image("phone_img")
+                                    .opacity(0.6)
+                                TextField("Номер телефона", text: $modelData.registrationData.phone)
+                                    .keyboardType(.phonePad)
+                                    .font(Font.custom("Montserrat-Meduim", size: 18))
+                                    .foregroundColor(Color(red: 0.22, green: 0.23, blue: 0.25))
+                                    .frame(width: 230)
+                            }
+                            Rectangle ()
+                                .frame(width: 280, height: 1)
+                                .foregroundColor(.clear)
+                                .background(Color(.black).opacity(0.4))
+                        }
+                        .padding(.horizontal)
+                        .padding(.bottom, 10)
+                        
+                        // Стек для пароля
+                        VStack (alignment: .leading){
+                            HStack (spacing: 10) {
+                                Image("pass_img")
+                            SecureField("Введите пароль", text: $modelData.registrationData.password)
+                                    .font(Font.custom("Montserrat-Meduim", size: 18))
+                                    .foregroundColor(Color(red: 0.22, green: 0.23, blue: 0.25))
+                                    .frame(width: 230)
+                            }
+                            Rectangle ()
+                                .frame(width: 280, height: 1)
+                                .foregroundColor(.clear)
+                                .background(Color(.black).opacity(0.4))
+                        }
+                        .padding(.horizontal)
+                        .padding(.bottom, 10)
+                        
+                        // Стек для подтверждения пароля
+                        VStack (alignment: .leading){
+                            HStack (spacing: 10) {
+                                Image("pass_img")
+                            SecureField("Подтвердите пароль", text: $modelData.registrationData.password)
+                                    .font(Font.custom("Montserrat-Meduim", size: 18))
+                                    .foregroundColor(Color(red: 0.22, green: 0.23, blue: 0.25))
+                                    .frame(width: 230)
+                            }
+                            Rectangle ()
+                                .frame(width: 280, height: 1)
+                                .foregroundColor(.clear)
+                                .background(Color(.black).opacity(0.4))
+                        }
+                        .padding(.horizontal)
+                        .padding(.bottom, 10)
+                        
+                        // Стек для кнопки
+                        Button(action: {
+                            print(modelData.registrationData.name, modelData.registrationData.gender, modelData.registrationData.birthDate, modelData.registrationData.email, modelData.registrationData.phone, modelData.registrationData.password)
+                        }
+                        ) {
+                            Text("Регистрация")
+                                .foregroundColor(Color(red: 0.22, green: 0.23, blue: 0.25))
+                                .font(.custom("Montserrat-Medium", size: 18))
+                                .padding(.vertical)
+                                .padding(.horizontal, 50)
+                                .background(Color(red: 1, green: 0.89, blue: 0.89).opacity(0.60))
+                                .clipShape(RoundedRectangle(cornerRadius: 15))
+                        }
+                        .padding(.top, 10)
+                    }
+                    .padding(.horizontal)
+                }
             }
+            
+            
         }
     }
 }
-
 
 #Preview {
     RegistrationView()
